3e27cb1c455f9572b5fb2c8127d9c222f32635b7
KallelGaNewk <kallelgn@gmail.com>
2024-12-26 00:36:17 -0300

    disable_esc_on_quote

---
+++ readme.md
@@ -0,0 +1,11 @@
+# arrowasd
+
+Program to bind WASD to arrow keys.
+
+Enable/disable with `AltGr + W`
+
+# Features
+###### Disable with `cargo build --no-default-features`
+
+## disable_esc_on_quote (default)
+Disable `Escape` for 300ms after typing `` ` ``.
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
         atomic::{AtomicBool, Ordering},
         Arc, Mutex,
     },
+    time::{Duration, SystemTime},
 };
 
 use lazy_static::lazy_static;
@@ -13,6 +14,9 @@ lazy_static! {
     static ref SWITCHING: AtomicBool = AtomicBool::new(false);
     static ref PRESSED_KEYS: Arc<Mutex<HashSet<Key>>> = Arc::new(Mutex::new(HashSet::new()));
     static ref BIND: HashSet<Key> = HashSet::from([Key::KeyW, Key::AltGr]);
+
+    #[cfg(feature = "disable_esc_on_quote")]
+    static ref ESC_DISABLED_UNTIL: Arc<Mutex<Option<SystemTime>>> = Arc::new(Mutex::new(None));
 }
 
 fn main() {
@@ -22,6 +26,21 @@ fn main() {
 }
 
 fn callback(event: Event) -> Option<Event> {
+    #[cfg(feature = "disable_esc_on_quote")]
+    {
+        let mut esc_disabled_until = ESC_DISABLED_UNTIL.lock().unwrap();
+
+        if let Some(disabled_until) = *esc_disabled_until {
+            if disabled_until > SystemTime::now() {
+                if let EventType::KeyPress(Key::Escape) = event.event_type {
+                    return None;
+                }
+            } else {
+                *esc_disabled_until = None;
+            }
+        }
+    }
+
     match event.event_type {
         EventType::KeyPress(key) => {
             let mut keys = PRESSED_KEYS.lock().unwrap();
@@ -29,6 +48,13 @@ fn callback(event: Event) -> Option<Event> {
 
             if keys.intersection(&BIND).count() == BIND.len() && BIND.contains(&key) {
                 SWITCHING.store(!SWITCHING.load(Ordering::SeqCst), Ordering::SeqCst);
+            }
+            println!("{key:?}");
+            #[cfg(feature = "disable_esc_on_quote")]
+            {
+                if key == Key::BackQuote {
+                    *ESC_DISABLED_UNTIL.lock().unwrap() = Some(SystemTime::now() + Duration::from_millis(300));
+                }
             }
         }
         EventType::KeyRelease(key) => {
